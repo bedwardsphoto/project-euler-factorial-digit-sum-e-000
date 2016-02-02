8d5263d8e84fe3c6d597c359c981831a3af32dbc
bedwardsphoto <brian@brianedwardsphotography.com>
2016-02-02 11:57:11 -0500

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,20 @@
-# code your solution here
+def factorial(n)
+  total=(1..n).inject(:*)
+ 
+end
+
+
+def sum_of_digits(n)
+sum=0
+single_digits=[]
+single_digits=(n.to_s).split('')
+single_digits.each do |i|
+sum+= i.to_i
+end
+return sum
+end
+
+def factorial_digit_sum(n)
+sum_of_digits(factorial(n))
+
+end
